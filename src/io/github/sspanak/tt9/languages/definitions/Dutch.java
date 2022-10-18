3b8acb7ad409f9c9510af2020f0de69cdd8366a2
TomJansen <tomjansen97@gmail.com>
2022-10-18 13:15:31 +0200

    Add Dutch language

---
+++ src/io/github/sspanak/tt9/languages/definitions/Dutch.java
@@ -0,0 +1,24 @@
+package io.github.sspanak.tt9.languages.definitions;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+import io.github.sspanak.tt9.R;
+
+public class Dutch extends English {
+	public Dutch() {
+		super();
+
+		id = 8;
+		name = "Nederlands";
+		locale = new Locale("nl","NL");
+		dictionaryFile = "nl-utf8.txt";
+		icon = R.drawable.ime_lang_nl;
+
+		characterMap.get(2).addAll(Arrays.asList("à", "ä", "ç"));
+		characterMap.get(3).addAll(Arrays.asList("é", "è", "ê", "ë"));
+		characterMap.get(4).addAll(Arrays.asList("î", "ï"));
+		characterMap.get(6).addAll(Arrays.asList("ö"));
+		characterMap.get(8).addAll(Arrays.asList("û", "ü"));
+	}
+}
