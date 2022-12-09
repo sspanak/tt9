55211b9cd9690f8c27db1e6e153b672fd118fae2
Dimo Karaivanov <doftor.livain@gmail.com>
2022-12-09 11:53:54 +0200

    Predictive Mode: fixed the text case seemingly not changing in some cases

--- src/io/github/sspanak/tt9/ime/TraditionalT9.java
+++ src/io/github/sspanak/tt9/ime/TraditionalT9.java
@@ -469,13 +469,24 @@ public class TraditionalT9 extends KeyPadHandler {
 		}
 		// when typing a word or viewing scrolling the suggestions, only change the case
 		else if (!isSuggestionViewHidden()) {
-			mInputMode.nextTextCase();
-			setSuggestions(mInputMode.getSuggestions(mLanguage), mSuggestionView.getCurrentIndex());
-			refreshComposingText();
+			String currentSuggestionBefore = getComposingText();
+
+			// When we are in AUTO mode and the dictionary word is in uppercase,
+			// the mode would switch to UPPERCASE, but visually, the word would not change.
+			// This is why we retry, until there is a visual change.
+			for (int retries = 0; retries < 2; retries ++) {
+				mInputMode.nextTextCase();
+				setSuggestions(mInputMode.getSuggestions(mLanguage), mSuggestionView.getCurrentIndex());
+				refreshComposingText();
+
+				if (!currentSuggestionBefore.equals(getComposingText())) {
+					break;
+				}
+			}
 		}
 		// make "abc" and "ABC" separate modes from user perspective
 		else if (mInputMode.isABC() && mInputMode.getTextCase() == InputMode.CASE_LOWER) {
-			mInputMode.setTextCase(InputMode.CASE_UPPER);
+			mInputMode.nextTextCase();
 		} else {
 			int modeIndex = (allowedInputModes.indexOf(mInputMode.getId()) + 1) % allowedInputModes.size();
 			mInputMode = InputMode.getInstance(settings, allowedInputModes.get(modeIndex));
