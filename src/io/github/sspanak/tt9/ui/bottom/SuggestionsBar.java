0c8cfb33ebc7678ac3ddd414ba3f19cb0c18d1e1
sspanak <doftor.livain@gmail.com>
2023-03-29 11:57:54 +0300

    fixed flash between suggestions and input mode label

--- src/io/github/sspanak/tt9/ui/bottom/SuggestionsBar.java
+++ src/io/github/sspanak/tt9/ui/bottom/SuggestionsBar.java
@@ -110,6 +110,8 @@ public class SuggestionsBar {
 
 	@SuppressLint("NotifyDataSetChanged")
 	public void setSuggestions(List<String> newSuggestions, int initialSel) {
+		ecoSetBackground(newSuggestions);
+
 		suggestions.clear();
 		selectedIndex = 0;
 
@@ -120,8 +122,6 @@ public class SuggestionsBar {
 			}
 			selectedIndex = Math.max(initialSel, 0);
 		}
-
-		setBackground();
 
 		mSuggestionsAdapter.setSelection(selectedIndex);
 		mSuggestionsAdapter.notifyDataSetChanged();
@@ -166,27 +166,51 @@ public class SuggestionsBar {
 		isDarkThemeEnabled = darkEnabled;
 		Context context = mView.getContext();
 
-
 		int defaultColor = darkEnabled ? R.color.dark_candidate_color : R.color.candidate_color;
 		int highlightColor = darkEnabled ? R.color.dark_candidate_selected : R.color.candidate_selected;
 
 		mSuggestionsAdapter.setColorDefault(ContextCompat.getColor(context, defaultColor));
 		mSuggestionsAdapter.setColorHighlight(ContextCompat.getColor(context, highlightColor));
 
-		setBackground();
+		setBackground(suggestions);
 	}
 
 
-	private void setBackground() {
-		int color = Color.TRANSPARENT;
-
-		if (suggestions.size() > 0) {
-			color = ContextCompat.getColor(
-				mView.getContext(),
-				isDarkThemeEnabled ? R.color.dark_candidate_background : R.color.candidate_background
-			);
+	/**
+	 * setBackground
+	 * Makes the background transparent, when there are no suggestions and theme-colored,
+	 * when there are suggestions.
+	 */
+	private void setBackground(List<String> newSuggestions) {
+		int newSuggestionsSize = newSuggestions != null ? newSuggestions.size() : 0;
+		if (newSuggestionsSize == 0) {
+			mView.setBackgroundColor(Color.TRANSPARENT);
+			return;
 		}
 
+		int color = ContextCompat.getColor(
+			mView.getContext(),
+			isDarkThemeEnabled ? R.color.dark_candidate_background : R.color.candidate_background
+		);
+
 		mView.setBackgroundColor(color);
+	}
+
+
+	/**
+	 * ecoSetBackground
+	 * A performance-optimized version of "setBackground().
+	 * Determines if the suggestions have changed and only then it changes the background.
+	 */
+	private void ecoSetBackground(List<String> newSuggestions) {
+		int newSuggestionsSize = newSuggestions != null ? newSuggestions.size() : 0;
+		if (
+			(newSuggestionsSize == 0 && suggestions.size() == 0)
+			|| (newSuggestionsSize > 0 && suggestions.size() > 0)
+		) {
+			return;
+		}
+
+		setBackground(newSuggestions);
 	}
 }
