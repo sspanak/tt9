d98372dcf34634765d6b60a520b8ed6b9a09a1db
sspanak <doftor.livain@gmail.com>
2023-04-24 19:41:50 +0300

    removed some unused code and imports

--- src/io/github/sspanak/tt9/languages/definitions/Spanish.java
+++ src/io/github/sspanak/tt9/languages/definitions/Spanish.java
@@ -2,7 +2,6 @@ package io.github.sspanak.tt9.languages.definitions;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Locale;
 
 import io.github.sspanak.tt9.languages.Characters;
