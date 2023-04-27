83945dd798a835efff3b727f9b519f879a31e917
DarthFlip <125198280+DarthFlip@users.noreply.github.com>
2023-04-27 20:47:43 +0300

    Create Yiddish.java

---
+++ src/io/github/sspanak/tt9/languages/definitions/Yiddish.java
@@ -0,0 +1,13 @@
+package io.github.sspanak.tt9.languages.definitions;
+
+import java.util.Locale;
+
+public class Yiddish extends Hebrew {
+	public Yiddish() {
+		super();
+
+		locale = Locale.YIDDISH;
+		dictionaryFile = "Yiddish-utf8.csv";
+
+	}
+}
