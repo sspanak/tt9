2c660bfe06b695514840d65576488a667d5557d9
sspanak <doftor.livain@gmail.com>
2023-05-15 11:39:51 +0300

    more clear name for Indonesian

--- src/io/github/sspanak/tt9/languages/definitions/Indonesian.java
+++ src/io/github/sspanak/tt9/languages/definitions/Indonesian.java
@@ -1,13 +1,13 @@
 package io.github.sspanak.tt9.languages.definitions;
 
-import java.util.Arrays;
 import java.util.Locale;
 
 public class Indonesian extends English {
 	public Indonesian() {
 		super();
 
-		locale = new Locale("in","ID");
+		name = "Bahasa Indonesia";
+		locale = new Locale("in", "ID");
 		dictionaryFile = "id-utf8.csv";
 	}
 }
