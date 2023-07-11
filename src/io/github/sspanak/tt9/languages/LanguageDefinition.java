0588df271546b380f259148bbedecbcce8f8a749
sspanak <doftor.livain@gmail.com>
2023-07-11 17:30:29 +0300

    SnakeYAML begone

--- src/io/github/sspanak/tt9/languages/LanguageDefinition.java
+++ src/io/github/sspanak/tt9/languages/LanguageDefinition.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.regex.Pattern;
+import java.util.Arrays;
 
 import io.github.sspanak.tt9.Logger;
 
@@ -25,6 +25,11 @@ public class LanguageDefinition {
 	public String locale = "";
 	public String name = "";
 
+
+	/**
+	 * getAllFiles
+	 * Returns a list of the paths of all language definition files in the assets folder or an empty list on error.
+	 */
 	public static ArrayList<String> getAllFiles(AssetManager assets) {
 		ArrayList<String> files = new ArrayList<>();
 		try {
@@ -41,11 +46,20 @@ public class LanguageDefinition {
 	}
 
 
+	/**
+	 * fromFile
+	 * Takes the path to a language definition in the assets folder and parses that file into a LanguageDefinition
+	 * or throws an IOException on error.
+	 */
 	public static LanguageDefinition fromFile(AssetManager assetManager, String definitionFile) throws IOException {
 		return parse(load(assetManager, definitionFile));
 	}
 
 
+	/**
+	 * load
+	 * Loads a language definition file from the assets folder into a String or throws an IOException on error.
+	 */
 	private static ArrayList<String> load(AssetManager assetManager, String definitionFile) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(assetManager.open(definitionFile), StandardCharsets.UTF_8));
 		ArrayList<String> fileContents = new ArrayList<>();
@@ -57,9 +71,14 @@ public class LanguageDefinition {
 		return fileContents;
 	}
 
+
 	/**
+	 * parse
 	 * Converts "yaml" to a LanguageDefinition object. All properties in the YAML are considered optional,
-	 * so the LanguageDefinition defaults will be used when no value is found in the YAML.
+	 * so the LanguageDefinition defaults will be used when some property is omitted.
+	 *
+	 * Had to write all this, because the only usable library, SnakeYAML, works fine on Android 10+,
+	 * but causes crashes on older devices.
 	 */
 	@NonNull
 	private static LanguageDefinition parse(ArrayList<String> yaml) {
@@ -78,7 +97,7 @@ public class LanguageDefinition {
 		value = getPropertyFromYaml(yaml, "name");
 		definition.name = value != null ? value : definition.name;
 
-		//		definition.layout = getLayoutFromYaml(yaml);
+		definition.layout = getLayoutFromYaml(yaml);
 
 		value = getPropertyFromYaml(yaml, "hasUpperCase");
 		if (value != null) {
@@ -113,40 +132,54 @@ public class LanguageDefinition {
 	}
 
 
-	private static ArrayList<ArrayList<String>> getLayoutFromYaml(String yaml) {
+	/**
+	 * getLayoutFromYaml
+	 * Finds and extracts the keypad layout. Less than 10 keys are accepted allowed leaving the ones up to 9-key empty.
+	 */
+	@NonNull
+	private static ArrayList<ArrayList<String>> getLayoutFromYaml(ArrayList<String> yaml) {
 		ArrayList<ArrayList<String>> layout = new ArrayList<>();
 
-		Pattern entireLayoutRegex = Pattern.compile("layout:((?:[^\\-]+?-\\s*\\[[^\\]]+\\]){0,10})", Pattern.DOTALL);
-		String rawLayout = entireLayoutRegex.matcher(yaml).group(0);
-		if (rawLayout == null) {
-			return layout;
-		}
-
-		rawLayout = rawLayout.trim();
-		String[] layoutLines = rawLayout.split("\\s+-");
-		if (layoutLines.length < 1) {
-			return layout;
-		}
-
-		for (String line : layoutLines) {
-			line = line
-				.trim()
-				.replaceAll("^\\s*\\[\\s*", "")
-				.replaceAll("#.+$", "")
-				.replaceAll("\\s*\\]\\s*$", "");
-
-			ArrayList<String> lineChars = new ArrayList<>();
-			for (String lineChar : line.split(",")) {
-				lineChar = lineChar.trim();
-				if (!lineChar.isEmpty()) {
-					lineChars.add(lineChar);
-				}
+		boolean inLayout = false;
+		for (int i = 0; i < yaml.size(); i++) {
+			if (yaml.get(i).contains("layout")) {
+				inLayout = true;
+				continue;
 			}
 
-			layout.add(lineChars);
+			if (inLayout) {
+				ArrayList<String> lineChars = getLayoutEntryFromYamlLine(yaml.get(i));
+				if (lineChars != null) {
+					layout.add(lineChars);
+				} else {
+					break;
+				}
+			}
 		}
 
 		return layout;
+	}
+
+
+	/**
+	 * getLayoutEntryFromYamlLine
+	 * Validates a YAML line as an array and returns the character list to be assigned to a given key (a layout entry).
+	 * If the YAML line is invalid, NULL will be returned.
+	 */
+	@Nullable
+	private static ArrayList<String> getLayoutEntryFromYamlLine(String yamlLine) {
+		if (!yamlLine.contains("[") || !yamlLine.contains("]")) {
+			return null;
+		}
+
+		String line = yamlLine
+			.replaceAll("#.+$", "")
+			.replace('-', ' ')
+			.replace('[', ' ')
+			.replace(']', ' ')
+			.replace(" ", "");
+
+		return new ArrayList<>(Arrays.asList(line.split(",")));
 	}
 
 
