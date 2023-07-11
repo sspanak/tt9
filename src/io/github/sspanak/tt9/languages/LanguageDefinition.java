5e521792ef2058b647c15c3f7a38dda2dbadc3d0
sspanak <doftor.livain@gmail.com>
2023-07-11 11:35:37 +0300

    stash

--- src/io/github/sspanak/tt9/languages/LanguageDefinition.java
+++ src/io/github/sspanak/tt9/languages/LanguageDefinition.java
@@ -2,13 +2,12 @@ package io.github.sspanak.tt9.languages;
 
 import android.content.res.AssetManager;
 
-import org.yaml.snakeyaml.Yaml;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 import io.github.sspanak.tt9.Logger;
 
@@ -29,6 +28,8 @@ public class LanguageDefinition {
 			for (String file : assets.list(definitionsDir)) {
 				files.add(definitionsDir + "/" + file);
 			}
+
+			Logger.d("LanguageDefinition", "Found: " + files.size() + " languages.");
 		} catch (IOException e) {
 			Logger.e("tt9.LanguageDefinition", "Failed reading language definitions from: '" + definitionsDir + "'. " + e.getMessage());
 		}
@@ -36,10 +37,91 @@ public class LanguageDefinition {
 		return files;
 	}
 
-	public static LanguageDefinition fromFile(AssetManager assets, String definitionFile) throws IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(assets.open(definitionFile), StandardCharsets.UTF_8));
-		return new Yaml().loadAs(reader, LanguageDefinition.class);
+
+	public static LanguageDefinition fromFile(AssetManager assetManager, String definitionFile) throws IOException {
+		return parse(load(assetManager, definitionFile));
 	}
+
+
+	private static String load(AssetManager assetManager, String definitionFile) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(assetManager.open(definitionFile), StandardCharsets.UTF_8));
+		StringBuilder fileContents = new StringBuilder();
+		String line;
+		while ((line = reader.readLine()) != null) {
+			fileContents.append(line).append("\n");
+		}
+
+		return fileContents.toString();
+	}
+
+
+	private static LanguageDefinition parse(String yaml) {
+		LanguageDefinition definition = new LanguageDefinition();
+
+		definition.abcString = getPropertyFromYaml(yaml, "absString");
+		definition.dictionaryFile = getPropertyFromYaml(yaml, "dictionaryFile");
+		definition.locale = getPropertyFromYaml(yaml, "locale");
+		definition.name = getPropertyFromYaml(yaml, "name");
+//		definition.layout = getLayoutFromYaml(yaml);
+//
+//		String hasUpperCase = getPropertyFromYaml(yaml, "hasUpperCase");
+//		if (hasUpperCase != null) {
+//			hasUpperCase = hasUpperCase.toLowerCase();
+//			definition.hasUpperCase = hasUpperCase.equals("true") || hasUpperCase.equals("on") || hasUpperCase.equals("yes") || hasUpperCase.equals("y");
+//		}
+
+		return definition;
+	}
+
+
+	private static String getPropertyFromYaml(String yaml, String property) {
+		try {
+			String regex = property + ":\\s*([^\\n]+)";
+			Pattern pattern = Pattern.compile(regex);
+			return pattern.matcher(yaml).group(0);
+		} catch (Exception e) {
+			Logger.w("LanguageDefinition", "Property '" + property + "' not found or invalid. " + e.getMessage());
+			return "";
+		}
+	}
+
+
+	private static ArrayList<ArrayList<String>> getLayoutFromYaml(String yaml) {
+		ArrayList<ArrayList<String>> layout = new ArrayList<>();
+
+		Pattern entireLayoutRegex = Pattern.compile("layout:((?:[^\\-]+?-\\s*\\[[^\\]]+\\]){0,10})", Pattern.DOTALL);
+		String rawLayout = entireLayoutRegex.matcher(yaml).group(0);
+		if (rawLayout == null) {
+			return layout;
+		}
+
+		rawLayout = rawLayout.trim();
+		String[] layoutLines = rawLayout.split("\\s+-");
+		if (layoutLines.length < 1) {
+			return layout;
+		}
+
+		for (String line : layoutLines) {
+			line = line
+				.trim()
+				.replaceAll("^\\s*\\[\\s*", "")
+				.replaceAll("#.+$", "")
+				.replaceAll("\\s*\\]\\s*$", "");
+
+			ArrayList<String> lineChars = new ArrayList<>();
+			for (String lineChar : line.split(",")) {
+				lineChar = lineChar.trim();
+				if (!lineChar.isEmpty()) {
+					lineChars.add(lineChar);
+				}
+			}
+
+			layout.add(lineChars);
+		}
+
+		return layout;
+	}
+
 
 	public String getDictionaryFile() {
 		return languagesDir + "/dictionaries/" + dictionaryFile;
