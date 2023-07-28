49eda37b990c1c6ce69d9d8b64cd91268af910b1
alexknop <knopalex@msu.edu>
2023-07-28 04:40:43 -0400

    removed the last of the legacy Backspace hack (#318)
    ---------
    
    Co-authored-by: Alex Knop <alexknoptech@protonmail.com>
    Co-authored-by: Dimo Karaivanov <doftor.livain@gmail.com>

--- src/io/github/sspanak/tt9/ime/KeyPadHandler.java
+++ src/io/github/sspanak/tt9/ime/KeyPadHandler.java
@@ -120,9 +120,7 @@ abstract class KeyPadHandler extends InputMethodService {
 
 		// "backspace" key must repeat its function when held down, so we handle it in a special way
 		if (Key.isBackspace(settings, keyCode)) {
-			// When there is no more text, allow "Back" key to function normally, not to block navigation.
-			// All other keys have their default function disabled.
-			isBackspaceHandled = onBackspace() || keyCode != KeyEvent.KEYCODE_BACK;
+			isBackspaceHandled = onBackspace();
 			return isBackspaceHandled;
 		} else {
 			isBackspaceHandled = false;
