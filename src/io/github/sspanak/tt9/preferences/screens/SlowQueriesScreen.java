8cd6137e85607dd6d3378114770487283984ac03
sspanak <doftor.livain@gmail.com>
2024-01-26 15:39:33 +0200

    slow queries can now be copied

--- src/io/github/sspanak/tt9/preferences/screens/SlowQueriesScreen.java
+++ src/io/github/sspanak/tt9/preferences/screens/SlowQueriesScreen.java
@@ -1,11 +1,17 @@
 package io.github.sspanak.tt9.preferences.screens;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.os.Build;
+
 import androidx.preference.Preference;
 
 import io.github.sspanak.tt9.Logger;
 import io.github.sspanak.tt9.R;
 import io.github.sspanak.tt9.db.SlowQueryStats;
 import io.github.sspanak.tt9.preferences.PreferencesActivity;
+import io.github.sspanak.tt9.ui.UI;
 
 public class SlowQueriesScreen extends BaseScreenFragment {
 	private final static String RESET_BUTTON = "pref_slow_queries_reset_stats";
@@ -22,6 +28,7 @@ public class SlowQueriesScreen extends BaseScreenFragment {
 	protected void onCreate() {
 		printSummary();
 		printSlowQueries();
+		enableLogsCopy();
 
 		Preference resetButton = findPreference(RESET_BUTTON);
 		if (resetButton != null) {
@@ -53,5 +60,22 @@ public class SlowQueriesScreen extends BaseScreenFragment {
 			}
 			queryListContainer.setSummary(message);
 		}
+	}
+
+
+	private void enableLogsCopy() {
+		Preference queryListContainer = findPreference(QUERY_LIST_CONTAINER);
+		if (activity == null || queryListContainer == null) {
+			return;
+		}
+
+		ClipboardManager clipboard = (ClipboardManager) activity.getSystemService(Context.CLIPBOARD_SERVICE);
+		queryListContainer.setOnPreferenceClickListener((Preference p) -> {
+			clipboard.setPrimaryClip(ClipData.newPlainText("TT9 debug log", p.getSummary()));
+			if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.S_V2) {
+				UI.toast(activity, "Logs copied.");
+			}
+			return true;
+		});
 	}
 }
