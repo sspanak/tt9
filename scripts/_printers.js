1e35e35d7784a3527931d95c842f76690c1a482b
sspanak <doftor.livain@gmail.com>
2024-04-08 19:14:29 +0300

    dictionary frequency normalization script

--- scripts/_printers.js
+++ scripts/_printers.js
@@ -3,5 +3,11 @@ exports.print = function(str) {
 };
 
 exports.printError = function(str) {
-	process.stderr.write(`${str}\n`);
+	process.stderr.write(`${str instanceof Error ? str.stack : str}\n`);
 };
+
+exports.printWordsWithFrequencies = function(words) {
+	if (Array.isArray(words)) {
+		words.forEach(w => exports.print(`${w.word}${w.frequency ? '\t' + w.frequency : ''}`));
+	}
+}
