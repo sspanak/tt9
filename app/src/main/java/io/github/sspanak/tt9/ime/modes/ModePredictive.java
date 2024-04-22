172ca4012ff38bc4bb120710476cb0ecf1c31444
sspanak <doftor.livain@gmail.com>
2024-04-22 10:45:17 +0300

    fixed v31.0 regression: holding a number key erases the composing text in Predicitve mode

--- app/src/main/java/io/github/sspanak/tt9/ime/modes/ModePredictive.java
+++ app/src/main/java/io/github/sspanak/tt9/ime/modes/ModePredictive.java
@@ -85,6 +85,7 @@ public class ModePredictive extends InputMode {
 			reset();
 			autoAcceptTimeout = 0;
 			disablePredictions = true;
+			digitSequence = String.valueOf(number);
 			suggestions.add(language.getKeyNumber(number));
 		} else {
 			super.reset();
