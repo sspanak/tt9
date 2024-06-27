9488eea7f8cc89a0b1045d830f2d005d5bcf92d8
sspanak <doftor.livain@gmail.com>
2024-06-27 15:28:57 +0300

    forgot to commit those

--- app/src/main/java/io/github/sspanak/tt9/preferences/settings/SettingsStore.java
+++ app/src/main/java/io/github/sspanak/tt9/preferences/settings/SettingsStore.java
@@ -13,7 +13,7 @@ public class SettingsStore extends SettingsUI {
 	public final static int DICTIONARY_DOWNLOAD_READ_TIMEOUT = 10000; // ms
 	public final static int DICTIONARY_IMPORT_BATCH_SIZE = 5000; // words
 	public final static int DICTIONARY_IMPORT_PROGRESS_UPDATE_TIME = 250; // ms
-	public final static int RESIZE_THROTTLING_TIME = 50; // ms
+	public final static int RESIZE_THROTTLING_TIME = 60; // ms
 	public final static byte SLOW_QUERY_TIME = 50; // ms
 	public final static int SOFT_KEY_DOUBLE_CLICK_DELAY = 500; // ms
 	public final static int SOFT_KEY_REPEAT_DELAY = 40; // ms
