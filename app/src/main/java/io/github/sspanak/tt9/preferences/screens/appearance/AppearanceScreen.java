44a0ca0f4c8946fe23198ba13e0da064dd6a4993
sspanak <doftor.livain@gmail.com>
2024-06-28 12:29:02 +0300

    numpad alignment setting

--- app/src/main/java/io/github/sspanak/tt9/preferences/screens/appearance/AppearanceScreen.java
+++ app/src/main/java/io/github/sspanak/tt9/preferences/screens/appearance/AppearanceScreen.java
@@ -26,7 +26,10 @@ public class AppearanceScreen extends BaseScreenFragment {
 		(new ItemStatusIcon(findPreference(ItemStatusIcon.NAME), activity.getSettings())).populate();
 		ItemHapticFeedback hapticFeedback = (new ItemHapticFeedback(findPreference(ItemHapticFeedback.NAME), activity.getSettings())).populate();
 
+		ItemNumpadAlignment numpadAlignment = new ItemNumpadAlignment(findPreference(ItemNumpadAlignment.NAME), activity.getSettings());
 		ItemNumpadKeyHeight numpadKeyHeight = new ItemNumpadKeyHeight(findPreference(ItemNumpadKeyHeight.NAME), activity.getSettings());
+
+
 		ItemDropDown[] items = {
 			new ItemSelectTheme(findPreference(ItemSelectTheme.NAME), activity),
 			new ItemSelectLayoutType(
@@ -35,9 +38,11 @@ public class AppearanceScreen extends BaseScreenFragment {
 				(layout) -> {
 					hapticFeedback.onLayoutChange(layout);
 					numpadKeyHeight.onLayoutChange(layout);
+					numpadAlignment.onLayoutChange(layout);
 				}),
 			new ItemSelectSettingsFontSize(findPreference(ItemSelectSettingsFontSize.NAME), this),
-			numpadKeyHeight
+			numpadKeyHeight,
+			numpadAlignment
 		};
 
 		for (ItemDropDown item : items) {
---
+++ app/src/main/java/io/github/sspanak/tt9/preferences/screens/appearance/ItemNumpadAlignment.java
@@ -0,0 +1,55 @@
+package io.github.sspanak.tt9.preferences.screens.appearance;
+
+import android.content.Context;
+import android.os.Build;
+import android.view.Gravity;
+
+import androidx.preference.DropDownPreference;
+
+import java.util.LinkedHashMap;
+
+import io.github.sspanak.tt9.R;
+import io.github.sspanak.tt9.preferences.items.ItemDropDown;
+import io.github.sspanak.tt9.preferences.settings.SettingsStore;
+
+public class ItemNumpadAlignment extends ItemDropDown {
+	public static final String NAME = "pref_numpad_alignment";
+
+	private final SettingsStore settings;
+
+	ItemNumpadAlignment(DropDownPreference item, SettingsStore settings) {
+		super(item);
+		this.settings = settings;
+	}
+
+	@Override
+	public ItemDropDown populate() {
+		if (item == null) {
+			return this;
+		}
+
+		Context context = item.getContext();
+
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+			item.setVisible(false);
+			return this;
+		}
+
+		LinkedHashMap<Integer, String> options = new LinkedHashMap<>();
+		options.put(Gravity.START, context.getString(R.string.virtual_numpad_alignment_left));
+		options.put(Gravity.CENTER_HORIZONTAL, context.getString(R.string.virtual_numpad_alignment_center));
+		options.put(Gravity.END, context.getString(R.string.virtual_numpad_alignment_right));
+
+		super.populateIntegers(options);
+		super.setValue(settings.getNumpadKeyAlignment() + "");
+		onLayoutChange(settings.getMainViewLayout());
+
+		return this;
+	}
+
+	void onLayoutChange(int mainViewLayout) {
+		if (item != null) {
+			item.setEnabled(mainViewLayout == SettingsStore.LAYOUT_NUMPAD);
+		}
+	}
+}
