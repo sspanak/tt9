2211a06f6878f94fc98161244775f9f76d61fd5b
sspanak <doftor.livain@gmail.com>
2024-12-21 17:34:02 +0200

    removed some more unused code

--- app/src/main/java/io/github/sspanak/tt9/db/sqlite/Migration.java
+++ app/src/main/java/io/github/sspanak/tt9/db/sqlite/Migration.java
@@ -3,27 +3,21 @@ package io.github.sspanak.tt9.db.sqlite;
 class Migration {
 	static final Migration[] LIST = {
 		new Migration(
-			"ALTER TABLE " + Tables.LANGUAGES_META + " ADD COLUMN fileHash TEXT NOT NULL DEFAULT 0",
-			true
+			"ALTER TABLE " + Tables.LANGUAGES_META + " ADD COLUMN fileHash TEXT NOT NULL DEFAULT 0"
 		),
 		new Migration(
-			"ALTER TABLE " + Tables.LANGUAGES_META + " RENAME COLUMN  normalizationPending TO _delete_me_0",
-			true
+			"ALTER TABLE " + Tables.LANGUAGES_META + " RENAME COLUMN  normalizationPending TO _delete_me_0"
 		),
 		new Migration(
-			"ALTER TABLE " + Tables.LANGUAGES_META + " ADD COLUMN positionsToNormalize TEXT NULL",
-			true
+			"ALTER TABLE " + Tables.LANGUAGES_META + " ADD COLUMN positionsToNormalize TEXT NULL"
 		),
 		new Migration(
-			"ALTER TABLE " + Tables.LANGUAGES_META + " ADD COLUMN maxWordsPerSequence INTEGER NOT NULL DEFAULT -1",
-			true
+			"ALTER TABLE " + Tables.LANGUAGES_META + " ADD COLUMN maxWordsPerSequence INTEGER NOT NULL DEFAULT -1"
 		)
 	};
 
 	final String query;
-	final boolean mayFail;
-	private Migration(String query, boolean mayFail) {
+	private Migration(String query) {
 		this.query = query;
-		this.mayFail = mayFail;
 	}
 }
