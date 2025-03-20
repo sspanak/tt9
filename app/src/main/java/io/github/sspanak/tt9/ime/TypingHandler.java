f06ee6f4b94845aea20eff81c1b093d3fdefb558
sspanak <doftor.livain@gmail.com>
2025-03-20 18:12:45 +0200

    added an option to disable the Predictive Mode alltogether

--- app/src/main/java/io/github/sspanak/tt9/ime/TypingHandler.java
+++ app/src/main/java/io/github/sspanak/tt9/ime/TypingHandler.java
@@ -296,6 +296,8 @@ public abstract class TypingHandler extends KeyPadHandler {
 		allowedInputModes = new ArrayList<>(inputType.determineInputModes(getApplicationContext()));
 		if (LanguageKind.isKorean(mLanguage)) {
 			allowedInputModes.remove((Integer) InputMode.MODE_ABC);
+		} else if (!settings.getPredictiveMode()) {
+			allowedInputModes.remove((Integer) InputMode.MODE_PREDICTIVE);
 		}
 
 		return InputModeValidator.validateMode(settings.getInputMode(), allowedInputModes);
