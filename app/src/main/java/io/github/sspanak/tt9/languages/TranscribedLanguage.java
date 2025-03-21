09c0a646dd74e2aa5b5367623be9aa724da53790
sspanak <doftor.livain@gmail.com>
2025-03-21 16:48:16 +0200

    fixed sorting of East Asian languages

--- app/src/main/java/io/github/sspanak/tt9/languages/TranscribedLanguage.java
+++ app/src/main/java/io/github/sspanak/tt9/languages/TranscribedLanguage.java
@@ -2,7 +2,7 @@ package io.github.sspanak.tt9.languages;
 
 import io.github.sspanak.tt9.util.TextTools;
 
-abstract class TranscribedLanguage extends Language {
+abstract class TranscribedLanguage extends Language implements Comparable<TranscribedLanguage> {
 
 	@Override
 	public boolean isValidWord(String word) {
@@ -12,5 +12,20 @@ abstract class TranscribedLanguage extends Language {
 
 		return (LanguageKind.isKorean(this) && TextTools.isHangul(word))
 			|| (LanguageKind.isChinese(this) && TextTools.isChinese(word));
+	}
+
+
+	/**
+	 * These are usually languages with special scripts, such as Chinese, Korean, etc. They can't
+	 * be sorted alphabetically so just put them at the end.
+	 */
+	protected String getSortingId() {
+		return getName();
+	}
+
+
+	@Override
+	public int compareTo(TranscribedLanguage other) {
+		return getSortingId().compareTo(other.getSortingId());
 	}
 }
