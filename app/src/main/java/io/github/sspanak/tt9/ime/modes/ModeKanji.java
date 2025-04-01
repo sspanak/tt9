bda791c7cc657addfc821e0b5d004b65ae021996
sspanak <doftor.livain@gmail.com>
2025-04-01 18:07:13 +0300

    japanese

---
+++ app/src/main/java/io/github/sspanak/tt9/ime/modes/ModeKanji.java
@@ -0,0 +1,26 @@
+package io.github.sspanak.tt9.ime.modes;
+
+import androidx.annotation.Nullable;
+
+import io.github.sspanak.tt9.hacks.InputType;
+import io.github.sspanak.tt9.ime.helpers.TextField;
+import io.github.sspanak.tt9.languages.Language;
+import io.github.sspanak.tt9.languages.LanguageKind;
+import io.github.sspanak.tt9.preferences.settings.SettingsStore;
+
+public class ModeKanji extends ModePinyin {
+	protected ModeKanji(SettingsStore settings, Language lang, InputType inputType, TextField textField) {
+		super(settings, lang, inputType, textField);
+		NAME = language.getName().replace(" / ローマ字", "");
+	}
+
+	@Override
+	public boolean changeLanguage(@Nullable Language newLanguage) {
+		if (LanguageKind.isJapanese(newLanguage)) {
+			setLanguage(newLanguage);
+			return true;
+		}
+
+		return false;
+	}
+}
