sspanak/traditional-t9

shell script language experimental

app/src/main/java/io/github/sspanak/tt9/hacks/InputType.java
@@ -190,7 +190,7 @@ public class InputType extends StandardInputType {
 	 */
 	@Override
 	public boolean isDefectiveText() {
-		return isDuoLingoReportBug() || isContactsAndroid15Field();
+		return isDuoLingoReportBug() || isContactsAndroid15Field() || isTermux();
 	}
 
 

sspanak/tt9

app/src/main/java/io/github/sspanak/tt9/hacks/InputType.java
@@ -195,7 +195,7 @@ public class InputType extends StandardInputType {
 	 */
 	@Override
 	public boolean isDefectiveText() {
-		return isDuoLingoReportBug() || isAndroid15ContactsField();
+		return isDuoLingoReportBug() || isAndroid15ContactsField() || isTermux();
 	}
 
 