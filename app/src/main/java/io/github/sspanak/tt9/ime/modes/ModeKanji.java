e17e8bf6ee3433884fc082f3e6fa37ccc92d2292
sspanak <doftor.livain@gmail.com>
2025-05-16 12:30:39 +0300

    fixed some Katakana words not being displayed correctly when filtering is on

--- app/src/main/java/io/github/sspanak/tt9/ime/modes/ModeKanji.java
+++ app/src/main/java/io/github/sspanak/tt9/ime/modes/ModeKanji.java
@@ -1,6 +1,9 @@
 package io.github.sspanak.tt9.ime.modes;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
 
 import io.github.sspanak.tt9.hacks.InputType;
 import io.github.sspanak.tt9.ime.helpers.TextField;
@@ -14,10 +17,28 @@ public class ModeKanji extends ModePinyin {
 		NAME = language.getName().replace(" / ローマ字", "");
 	}
 
+
+	@NonNull
 	@Override
-	public boolean validateLanguage(@Nullable Language newLanguage) {
-		return LanguageKind.isJapanese(newLanguage);
+	public ArrayList<String> getSuggestions() {
+		ArrayList<String> newSuggestions = new ArrayList<>();
+		for (String s : suggestions) {
+			// "Ql" is the transcription of "—" in the database, as defined in Japanese.yml. However, this
+			// has only technical meaning. When displaying the suggestions, we want to show "—" for better
+			// readability.
+			newSuggestions.add(s.replaceAll("Ql", "—"));
+		}
+
+		return newSuggestions;
 	}
+
+
+	@Override
+	public boolean onReplaceSuggestion(@NonNull String word) {
+		// revert to the transcription, so that filtering works correctly
+		return super.onReplaceSuggestion(word.replaceAll("—", "Ql"));
+	}
+
 
 	@Override
 	public boolean shouldAcceptPreviousSuggestion(int nextKey, boolean hold) {
@@ -31,5 +52,11 @@ public class ModeKanji extends ModePinyin {
 		}
 
 		return super.shouldAcceptPreviousSuggestion(nextKey, hold);
+	}
+
+
+	@Override
+	public boolean validateLanguage(@Nullable Language newLanguage) {
+		return LanguageKind.isJapanese(newLanguage);
 	}
 }
