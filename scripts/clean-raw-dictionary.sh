917d211f1bd0172decf6ef4b3e5f46ed75f70955
sspanak <doftor.livain@gmail.com>
2025-05-31 15:16:03 +0300

    clean-raw-dictionary: removed the ability to filter using a corpus whitelist due to the low quality of the words

--- scripts/clean-raw-dictionary.sh
+++ scripts/clean-raw-dictionary.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ $# -lt 8 ]; then
-	echo "Usage: $0 <raw-word-list.txt> <output.txt> <whitelist.txt> <suffixes.txt> <hunspell.aff> <hunspell.dic> <allowed-lowercase-char-list> <allowed-uppercase-char-list>"
+if [ $# -lt 7 ]; then
+	echo "Usage: $0 <raw-word-list.txt> <output.txt> <suffixes.txt> <hunspell.aff> <hunspell.dic> <allowed-lowercase-char-list> <allowed-uppercase-char-list>"
 	echo
 	echo "Example (Slovak, no need of whitelist filter):"
-	echo "  $0 sk-raw.txt sk-filtered.txt /dev/null /dev/null sk_SK.aff sk_SK.dic aáäbcčdďeéfghiíjklĺľmnňoóôpqrŕsštťuúvwxyýzž AÁÄBCČDĎEÉFGHIÍJKLĹĽMNŇOÓÔPQRŔSŠTŤUÚVWXYÝZŽ"
+	echo "  $0 sk-raw.txt sk-filtered.txt /dev/null sk_SK.aff sk_SK.dic aáäbcčdďeéfghiíjklĺľmnňoóôpqrŕsštťuúvwxyýzž AÁÄBCČDĎEÉFGHIÍJKLĹĽMNŇOÓÔPQRŔSŠTŤUÚVWXYÝZŽ"
 	echo
 	echo "Example (Slovak, with whitelist filter):"
-	echo "  $0 sk-raw.txt sk-filtered.txt cc-100+uni-leipzig.txt sk-suffix.txt sk_SK.aff sk_SK.dic aáäbcčdďeéfghiíjklĺľmnňoóôpqrŕsštťuúvwxyýzž AÁÄBCČDĎEÉFGHIÍJKLĹĽMNŇOÓÔPQRŔSŠTŤUÚVWXYÝZŽ"
+	echo "  $0 sk-raw.txt sk-filtered.txt sk-suffix.txt sk_SK.aff sk_SK.dic aáäbcčdďeéfghiíjklĺľmnňoóôpqrŕsštťuúvwxyýzž AÁÄBCČDĎEÉFGHIÍJKLĹĽMNŇOÓÔPQRŔSŠTŤUÚVWXYÝZŽ"
 
 	exit 1
 fi
@@ -18,25 +18,23 @@ if ! [[ -f $DICTIONARY_FILE ]]; then
 	exit 2
 fi
 
-SUFFIXES_FILE=$4
+SUFFIXES_FILE=$3
 
-
-AFF_FILE=$5
+AFF_FILE=$4
 if ! [[ -f "$AFF_FILE" ]]; then
-    echo "whitelist file: '$AFF_FILE' does not exist"
-    exit 2
+	echo ".aff file: '$AFF_FILE' does not exist"
+	exit 2
 fi
 
-DIC_FILE=$6
+DIC_FILE=$5
 if ! [[ -f "$DIC_FILE" ]]; then
-    echo "whitelist file: '$DIC_FILE' does not exist"
-    exit 2
+	echo ".dic file: '$DIC_FILE' does not exist"
+	exit 2
 fi
 
 OUTPUT_FILE=$2
-WHITELIST_FILE=$3
-ALLOWED_LOWERCASE_CHARS=$7
-ALLOWED_UPPERCASE_CHARS=$8
+ALLOWED_LOWERCASE_CHARS=$6
+ALLOWED_UPPERCASE_CHARS=$7
 WORK_DIR="/tmp/TT9_$(uuidgen)"
 SCRIPT_DIR="$(dirname "$0")"
 
@@ -45,27 +43,21 @@ if ! [[ -d $SCRIPT_DIR/venv ]]; then
 fi
 
 
-whitelist_filter() {
+generate_words() {
 	CLEAN_WORDS=$1
 	OUTPUT=$2
 	DICTIONARY=${AFF_FILE::-4}
 
-	if ! [[ -f "$WHITELIST_FILE" ]]; then
-	    echo "Whitelist file: '$WHITELIST_FILE' does not exist. Skipping filtering."
-	    cp $CLEAN_WORDS $OUTPUT
-	    return
-	fi
-
 	if ! [[ -f "$SUFFIXES_FILE" ]]; then
-	    echo "Suffixes file: '$SUFFIXES_FILE' does not exist. Skipping filtering."
-	    cp $CLEAN_WORDS $OUTPUT
-	    return
+		echo "Suffixes file: '$SUFFIXES_FILE' does not exist. Skipping extra word generation."
+		cp $CLEAN_WORDS $OUTPUT
+		return
 	fi
 
-	echo "Preparing to filter words using whitelist file $WHITELIST_FILE." \
-		&& cat $WHITELIST_FILE | sort -u | uniq > $WORK_DIR/whitelist-uniq.txt \
-		&& python $SCRIPT_DIR/whitelist-filter.py $WORK_DIR/whitelist-uniq.txt $CLEAN_WORDS $SUFFIXES_FILE $WORK_DIR/whitelisted-raw.txt \
-		&& printf "Validating the whitelisted words with Hunspell... " && hunspell -i UTF-8 -G -d "$DICTIONARY" $WORK_DIR/whitelisted-raw.txt > $OUTPUT && echo "OK"
+	printf "Extracting valid words for generating new ones... " && hunspell -i UTF-8 -G -d "$DICTIONARY" $CLEAN_WORDS | sort -u | uniq > $WORK_DIR/generation-stems.txt && echo "OK" \
+	&& python $SCRIPT_DIR/generate-words-from-suffixes.py $WORK_DIR/generation-stems.txt $SUFFIXES_FILE $WORK_DIR/generated-raw.txt \
+	&& printf "Validating generated words with Hunspell... " && hunspell -i UTF-8 -G -d "$DICTIONARY" $WORK_DIR/generated-raw.txt > $WORK_DIR/generated-valid.txt && echo "OK" \
+	&& printf "Merging generated and input words... " && cat $CLEAN_WORDS $WORK_DIR/generated-valid.txt | sort -u | uniq > $OUTPUT && echo "OK"
 }
 
 # remove Roman numerals: ^(M{0,3})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$
@@ -78,8 +70,8 @@ mkdir -p $WORK_DIR \
 	&& printf "Removing UPPERlower... " && grep -vE "[$ALLOWED_UPPERCASE_CHARS]{2,}[$ALLOWED_LOWERCASE_CHARS]" $WORK_DIR/no_low_up.txt > $WORK_DIR/no_up_low.txt && echo "OK" \
 	&& printf "Removing single chars... " && grep -vE "^.$" $WORK_DIR/no_up_low.txt > $WORK_DIR/no_single.txt && echo "OK" \
 	&& printf "Removing words with repeeeeaaaated letters... " && grep -vE "(.)\1{2,}" $WORK_DIR/no_single.txt | grep -vE "^(.)\1$" | sort | uniq > $WORK_DIR/no_multi.txt && echo "OK" \
-	&& whitelist_filter $WORK_DIR/no_multi.txt $WORK_DIR/whitelisted.txt \
-	&& echo "Preparing to fix the text case." && source $SCRIPT_DIR/venv/bin/activate && python $SCRIPT_DIR/fix-text-case.py $WORK_DIR/whitelisted.txt $WORK_DIR/text_case.txt --aff "$AFF_FILE" --dic "$DIC_FILE" \
+	&& generate_words $WORK_DIR/no_multi.txt $WORK_DIR/generated.txt \
+	&& echo "Preparing to fix the text case." && source $SCRIPT_DIR/venv/bin/activate && python $SCRIPT_DIR/fix-text-case.py $WORK_DIR/generated.txt $WORK_DIR/text_case.txt --aff "$AFF_FILE" --dic "$DIC_FILE" \
 	&& INITIAL_COUNT=$(wc -l < "$DICTIONARY_FILE") && FINAL_COUNT=$(wc -l < "$WORK_DIR/text_case.txt") && echo "Word count: $INITIAL_COUNT -> $FINAL_COUNT" \
 	&& mv $WORK_DIR/text_case.txt "$OUTPUT_FILE"
 
