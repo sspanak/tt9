465145318e6180b9feea56f030a3b843e27c48b8
sspanak <doftor.livain@gmail.com>
2025-06-27 16:43:43 +0300

    icons and labels

--- app/src/main/java/io/github/sspanak/tt9/languages/LocaleCompat.java
+++ app/src/main/java/io/github/sspanak/tt9/languages/LocaleCompat.java
@@ -56,7 +56,11 @@ class LocaleCompat {
 			case "th" -> "ไท";
 			case "uk" -> "ук";
 			case "zgh" -> "dz".equals(country) ? "tm" : "ⵜⵎ";
-			case "zh" -> "cn".equals(country) ? "拼" : language;
+			case "zh" -> switch (country) {
+				case "cn" -> "拼";
+				case "tw" -> "注";
+				default -> country;
+			};
 			case "ca", "ga", "sw" -> language;
 			default -> country;
 		};
