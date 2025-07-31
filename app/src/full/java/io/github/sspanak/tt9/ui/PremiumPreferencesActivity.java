0ccbd89728dc8322dd79138a22f98e23f8219e0d
sspanak <doftor.livain@gmail.com>
2025-07-31 17:17:04 +0300

    code restructuring to add support for separate source sets from different repos

---
+++ app/src/full/java/io/github/sspanak/tt9/ui/PremiumPreferencesActivity.java
@@ -0,0 +1,17 @@
+package io.github.sspanak.tt9.ui;
+
+import androidx.annotation.Nullable;
+
+import io.github.sspanak.tt9.preferences.PreferencesActivity;
+import io.github.sspanak.tt9.preferences.screens.BaseScreenFragment;
+import io.github.sspanak.tt9.preferences.screens.main.MainSettingsScreen;
+
+/**
+ * Implemented in the "premium" source set. The open-source version
+ * has no premium features, so this class has only minimal functionality.
+ */
+public class PremiumPreferencesActivity extends ActivityWithNavigation {
+	protected BaseScreenFragment getScreen(PreferencesActivity prefsActivity, @Nullable String ignored) {
+		return new MainSettingsScreen(prefsActivity);
+	}
+}
