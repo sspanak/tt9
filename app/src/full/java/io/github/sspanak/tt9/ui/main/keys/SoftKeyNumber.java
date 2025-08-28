9ac8ffc3a0b509df83719e5b22db717ac9029904
sspanak <doftor.livain@gmail.com>
2025-08-28 17:31:32 +0300

    fix number

--- app/src/full/java/io/github/sspanak/tt9/ui/main/keys/SoftKeyNumber.java
+++ app/src/full/java/io/github/sspanak/tt9/ui/main/keys/SoftKeyNumber.java
@@ -3,7 +3,7 @@ package io.github.sspanak.tt9.ui.main.keys;
 import android.content.Context;
 import android.util.AttributeSet;
 
-public class SoftKeyNumber extends SoftKeyNumpadTextEditing {
+public class SoftKeyNumber extends SoftKeyTextEditingNumpad {
 	public SoftKeyNumber(Context context) { super(context); }
 	public SoftKeyNumber(Context context, AttributeSet attrs) { super(context, attrs); }
 	public SoftKeyNumber(Context context, AttributeSet attrs, int defStyleAttr) { super(context, attrs, defStyleAttr); }
