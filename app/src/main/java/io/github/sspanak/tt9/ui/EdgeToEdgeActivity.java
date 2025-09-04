d986ac8029d0d28998fc38044680faaf4cf6427d
sspanak <doftor.livain@gmail.com>
2025-09-04 14:45:51 +0300

    upgraded com.android.application 8.12.1 -> 8.12.2; upgraded com.google.android.material:material 1.12.0 -> 1.13.0

--- app/src/main/java/io/github/sspanak/tt9/ui/EdgeToEdgeActivity.java
+++ app/src/main/java/io/github/sspanak/tt9/ui/EdgeToEdgeActivity.java
@@ -1,15 +1,20 @@
 package io.github.sspanak.tt9.ui;
 
+import android.util.TypedValue;
 import android.view.View;
 import android.view.WindowInsets;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import io.github.sspanak.tt9.util.sys.DeviceInfo;
 
-
 public class EdgeToEdgeActivity extends AppCompatActivity {
-	public void preventEdgeToEdge(View view) {
+	/**
+	 * Adds padding to prevent the Preferences from going under the System Navigation Bar,
+	 * the Status Bar, and other surrounding elements. Otherwise, they become hard to reach and see.
+	 */
+	public void preventEdgeToEdge(@Nullable View view) {
 		if (view == null || !DeviceInfo.AT_LEAST_ANDROID_15) {
 			return;
 		}
@@ -21,11 +26,12 @@ public class EdgeToEdgeActivity extends AppCompatActivity {
 
 		view.setPadding(
 			insets.getStableInsetLeft(),
-			insets.getStableInsetTop(),
+			insets.getStableInsetTop() + getActionBarHeight(),
 			insets.getStableInsetRight(),
 			insets.getStableInsetBottom()
 		);
 	}
+
 
 	@Override
 	public void onAttachedToWindow() {
@@ -33,11 +39,13 @@ public class EdgeToEdgeActivity extends AppCompatActivity {
 		applyThemeToSystemUi();
 	}
 
+
 	private void applyThemeToSystemUi() {
 		if (DeviceInfo.AT_LEAST_ANDROID_10) {
 			getWindow().setStatusBarContrastEnforced(true);
 		}
 	}
+
 
 	private WindowInsets getInsets(View view) {
 		if (!DeviceInfo.AT_LEAST_ANDROID_6) {
@@ -46,5 +54,20 @@ public class EdgeToEdgeActivity extends AppCompatActivity {
 
 		WindowInsets newInsets = view != null ? view.getRootWindowInsets() : null;
 		return newInsets == null ? getWindow().getDecorView().getRootWindowInsets() : newInsets;
+	}
+
+
+	/**
+	 * Since com.google.android.material:material 1.13.0, the Title Bar appears over the Preferences.
+	 * Here we calculate its height so we can add padding to the top of the Preferences list.
+	 */
+	private int getActionBarHeight() {
+		int actionBarHeight = 0;
+		TypedValue tv = new TypedValue();
+		if (getTheme().resolveAttribute(androidx.appcompat.R.attr.actionBarSize, tv, true)) {
+			actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data, getResources().getDisplayMetrics());
+		}
+
+		return actionBarHeight;
 	}
 }
