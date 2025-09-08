b764fd0251c4aeed136413e3a4b8927108a83e12
sspanak <doftor.livain@gmail.com>
2025-09-08 17:41:29 +0300

    code cleanup

--- app/src/main/java/io/github/sspanak/tt9/preferences/screens/appearance/ItemShowArrows.java
+++ app/src/main/java/io/github/sspanak/tt9/preferences/screens/appearance/ItemShowArrows.java
@@ -7,7 +7,7 @@ import io.github.sspanak.tt9.preferences.settings.SettingsStore;
 
 public class ItemShowArrows extends ItemSwitch implements ItemLayoutChangeReactive {
 	public final static String NAME = "pref_arrow_keys_visible";
-	private final SettingsStore settings;
+	protected final SettingsStore settings;
 
 	public ItemShowArrows(SwitchPreferenceCompat item, SettingsStore settings) {
 		super(item);
