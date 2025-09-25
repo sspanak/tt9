f20fa797f2333e2359374f2835329cb080d4959a
sspanak <doftor.livain@gmail.com>
2025-09-25 12:15:30 +0300

    renamed SoftKeyPunctuation -> SoftKeyText

---
+++ app/src/full/java/io/github/sspanak/tt9/ui/main/keys/SoftKeyTextRightPremium.java
@@ -0,0 +1,14 @@
+package io.github.sspanak.tt9.ui.main.keys;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Implemented in the "premium" source set. The open-source version
+ * has no premium features, so this class is empty.
+ */
+public class SoftKeyTextRightPremium extends SoftKeyTextRight {
+	public SoftKeyTextRightPremium(Context context) { super(context); }
+	public SoftKeyTextRightPremium(Context context, AttributeSet attrs) { super(context, attrs); }
+	public SoftKeyTextRightPremium(Context context, AttributeSet attrs, int defStyleAttr) { super(context, attrs, defStyleAttr); }
+}
