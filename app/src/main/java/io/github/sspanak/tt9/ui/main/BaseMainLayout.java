3e77c30d3d7a186580d0cfecb058900da63ef50a
Atticus Finley <71795010+beackers@users.noreply.github.com>
2025-09-27 09:11:00 -0500

    Defined showDeveloperCommands(), added to BaseMainLayout Found the base for the showDeveloperCommands() function (I think?) and added developer commands to that Decided to explore where BaseMainLayout went and found some stuff about the command palette, and added the developer commands stuff

--- app/src/main/java/io/github/sspanak/tt9/ui/main/BaseMainLayout.java
+++ app/src/main/java/io/github/sspanak/tt9/ui/main/BaseMainLayout.java
@@ -326,11 +326,13 @@ abstract public class BaseMainLayout {
 	}
 
 
+	abstract void showDeveloperCommands();
 	abstract void showCommandPalette();
 	abstract void showKeyboard();
 	abstract void showTextEditingPalette();
 	abstract boolean isCommandPaletteShown();
 	abstract boolean isTextEditingPaletteShown();
+	abstract boolean isDeveloperCommandsShown();
 
 
 	/**
@@ -339,7 +341,7 @@ abstract public class BaseMainLayout {
 	 */
 
 	protected boolean isFnPanelVisible() {
-		return isCommandPaletteShown() || isTextEditingPaletteShown();
+		return isCommandPaletteShown() || isTextEditingPaletteShown() || isDeveloperCommandsShown();
 	}
 
 
