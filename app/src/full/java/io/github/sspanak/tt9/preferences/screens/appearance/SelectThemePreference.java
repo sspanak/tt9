0d9d0a4bdf3fddc241a45fc782652a5434a47f80
sspanak <doftor.livain@gmail.com>
2025-10-06 13:07:21 +0300

    removed some unused code

--- app/src/full/java/io/github/sspanak/tt9/preferences/screens/appearance/SelectThemePreference.java
+++ app/src/full/java/io/github/sspanak/tt9/preferences/screens/appearance/SelectThemePreference.java
@@ -5,14 +5,6 @@ import android.util.AttributeSet;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatDelegate;
-import androidx.preference.DropDownPreference;
-import androidx.preference.Preference;
-
-import java.util.LinkedHashMap;
-
-import io.github.sspanak.tt9.R;
-import io.github.sspanak.tt9.preferences.settings.SettingsStore;
 
 public class SelectThemePreference extends DarkThemePreference {
 	public SelectThemePreference(@NonNull Context context) { super(context); }
