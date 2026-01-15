8866deb54ec195de5f412e745a79b3dec33400d1
sspanak <doftor.livain@gmail.com>
2026-01-15 20:50:07 +0200

    no more hardcoded key IDs for the commands

--- app/src/main/java/io/github/sspanak/tt9/commands/CmdShowSettings.java
+++ app/src/main/java/io/github/sspanak/tt9/commands/CmdShowSettings.java
@@ -2,14 +2,19 @@ package io.github.sspanak.tt9.commands;
 
 import androidx.annotation.Nullable;
 
+import io.github.sspanak.tt9.R;
 import io.github.sspanak.tt9.ime.TraditionalT9;
 
 public class CmdShowSettings implements Command {
 	public static final String ID = "key_show_settings";
-	public String getId() { return ID; }
-	public int getIcon() { return io.github.sspanak.tt9.R.drawable.ic_fn_settings; }
-	public int getName() { return io.github.sspanak.tt9.R.string.function_show_settings; }
-	public boolean run(@Nullable TraditionalT9 tt9) {
+	@Override public String getId() { return ID; }
+	@Override public int getIcon() { return R.drawable.ic_fn_settings; }
+	@Override public int getName() { return R.string.function_show_settings; }
+	@Override public int getHardKey() { return 9; }
+	@Override public int getPaletteKey() { return R.id.soft_key_9; }
+
+
+	@Override public boolean run(@Nullable TraditionalT9 tt9) {
 		if (tt9 != null) {
 			tt9.showSettings();
 			return true;
--- app/src/main/java/io/github/sspanak/tt9/commands/Command.java
+++ app/src/main/java/io/github/sspanak/tt9/commands/Command.java
@@ -8,10 +8,44 @@ import androidx.annotation.Nullable;
 import io.github.sspanak.tt9.ime.TraditionalT9;
 
 public interface Command {
+	/**
+	 * The unique command identifier. It could be used for saving user preferences, for example "key_add_word".
+	 */
 	String getId();
+
+	/**
+	 * The drawable resource id for the command icon.
+	 */
 	int getIcon();
+
+	/**
+	 * The string resource id for the command name, displayed in the Settings.
+	 */
 	int getName();
+
+	/**
+	 * Same as getName(), but returns the actual string.
+	 */
 	default String getName(@NonNull Context context) { return context.getString(getName()); }
+
+	/**
+	 * The hard key code (KeyEvent.KEYCODE_*) that triggers this command, or -1 if none.
+	 */
+	default int getHardKey() { return -1; }
+
+	/**
+	 * The id of the command-palette key (R.id.soft_key_*) that triggers this command, or 0 if none.
+	 */
+	default int getPaletteKey() { return 0; }
+
+	/**
+	 * Returns true if the command is available. Useful for enabling/disabling SoftKeys.
+	 */
 	default boolean isAvailable(@Nullable TraditionalT9 tt9) { return true; }
+
+	/**
+	 * Executes the command action.
+	 * @return true if the command was executed successfully, false otherwise.
+	 */
 	default boolean run(@Nullable TraditionalT9 tt9) { return false; }
 }
