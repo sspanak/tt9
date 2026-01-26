75293ce4886d219478db83f20556f9fc7f706274
sspanak <doftor.livain@gmail.com>
2026-01-26 12:22:14 +0200

    fixed the clipboard list remaining visible after sending pasted text in messaging apps

--- app/src/main/java/io/github/sspanak/tt9/hacks/AppHacks.java
+++ app/src/main/java/io/github/sspanak/tt9/hacks/AppHacks.java
@@ -234,7 +234,7 @@ public class AppHacks {
 	 * and reset the InputMode upon sending a message.
 	 */
 	private boolean acceptComposingTextOnCursorReset(@NonNull InputMode inputMode, @NonNull SuggestionOps suggestionOps, @Nullable TextField textField) {
-		if (!isComposingCausingRestarts() && textField != null && textField.isEmpty() && !inputMode.getSuggestions().isEmpty()) {
+		if (!isComposingCausingRestarts() && textField != null && textField.isEmpty() && !(inputMode.getSuggestions().isEmpty() && suggestionOps.isEmpty())) {
 			inputMode.onAcceptSuggestion(suggestionOps.acceptIncomplete());
 			inputMode.reset();
 			return true;
