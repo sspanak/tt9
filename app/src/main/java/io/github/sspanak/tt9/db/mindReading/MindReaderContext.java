350da019a098537088fba461bebbe78201c1d0be
sspanak <doftor.livain@gmail.com>
2026-02-03 19:03:13 +0200

    build a dictionary

--- app/src/main/java/io/github/sspanak/tt9/db/mindReading/MindReaderContext.java
+++ app/src/main/java/io/github/sspanak/tt9/db/mindReading/MindReaderContext.java
@@ -3,57 +3,80 @@ package io.github.sspanak.tt9.db.mindReading;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import io.github.sspanak.tt9.util.Logger;
+import java.util.Arrays;
 
-public class MindReaderContext {
-	private static final String LOG_TAG = MindReaderContext.class.getSimpleName();
+import io.github.sspanak.tt9.languages.Language;
+import io.github.sspanak.tt9.languages.LanguageKind;
 
-	private String mockContext;
-	private final String[] buffer = new String[4]; // @todo: convert this to int
-	private int size = 0;
+class MindReaderContext {
+	@Nullable Language language;
+	@NonNull MindReaderDictionary dictionary;
+
+	private final int maxTokens;
+	@NonNull private String rawContext;
+	@NonNull private int[] tokenContext = new int[0];
 
 
-	boolean isEmpty() {
-		return mockContext == null || mockContext.isEmpty();
+	MindReaderContext(@NonNull MindReaderDictionary dictionary, int maxTokens) {
+		this.dictionary = dictionary;
+		this.maxTokens = maxTokens;
+		rawContext = "";
 	}
 
-	public boolean set(@Nullable String beforeCursor) {
+
+	void setLanguage(@NonNull Language language, @NonNull MindReaderDictionary dictionary) {
+		this.dictionary = dictionary;
+		this.language = language;
+	}
+
+
+	boolean setText(@Nullable String beforeCursor) {
 		final boolean isBeforeEmpty = beforeCursor == null || beforeCursor.isEmpty();
-		if (isEmpty() && isBeforeEmpty) {
+		if (rawContext.isEmpty() && isBeforeEmpty) {
 			return false;
 		}
 
-		mockContext = beforeCursor;
-		Logger.d(LOG_TAG, "========> set context: " + this);
+		rawContext = beforeCursor == null ? "" : beforeCursor;
 
 		return true;
 	}
 
-//	public void add(@Nullable String word) {
-//		if (word == null || word.isEmpty()) {
-//			Logger.d(LOG_TAG, "Not adding an empty word to the MindReader.");
-//			return;
-//		}
-//
-//		// @todo: convert the words to database positions.
-//		//  - Allow only the Top64k words. The rest are noise.
-//		//  - Punctuation IDs are hardcoded at ID > 10^7
-//		if (size < 4) {
-//			buffer[size++] = word;
-//		} else {
-//			buffer[0] = buffer[1];
-//			buffer[1] = buffer[2];
-//			buffer[2] = buffer[3];
-//			buffer[3] = word;
-//		}
-//
-//		Logger.d(LOG_TAG, "======> mind-reading context: " + this);
-//	}
+
+	void process() {
+		final String[] newTokens = filterUnpopularTokens(tokenize());
+		dictionary.addMany(newTokens);
+		setTokenContext(newTokens);
+	}
+
+
+	private String[] filterUnpopularTokens(String[] tokens) {
+		// @todo: filter using the database
+		return tokens;
+	}
+
+
+	private void setTokenContext(String[] newTokens) {
+		tokenContext = new int[newTokens.length];
+		for (int i = 0; i < newTokens.length; i++) {
+			tokenContext[i] = dictionary.indexOf(newTokens[i]);
+		}
+	}
+
+
+	private String[] tokenize() {
+		final boolean isLanguageHebrew = LanguageKind.isHebrew(language);
+		return ContextTokenizer.tokenize(
+			rawContext,
+			maxTokens,
+			LanguageKind.isUkrainian(language) || isLanguageHebrew,
+			isLanguageHebrew
+		);
+	}
+
 
 	@Override
 	@NonNull
 	public String toString() {
-		return mockContext;
-//		return java.util.Arrays.toString(buffer);
+		return "raw=\"" + rawContext + "\" + tokens=" + Arrays.toString(tokenContext) + " in dict=" + dictionary;
 	}
 }
