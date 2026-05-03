dded6ba5bda5f04ef583d6b24298f3adf293ed06
sspanak <doftor.livain@gmail.com>
2026-05-03 22:35:35 +0300

    added accessibility announcements for the Fn keys

--- app/src/main/java/io/github/sspanak/tt9/commands/NullCommand.java
+++ app/src/main/java/io/github/sspanak/tt9/commands/NullCommand.java
@@ -2,12 +2,12 @@ package io.github.sspanak.tt9.commands;
 
 import android.content.Context;
 
-import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public class NullCommand implements Command {
 	public static final String ID = "null";
 	@Override public String getId() { return ID; }
 	@Override public int getIcon() { return -1; }
 	@Override public int getName() { return 0; }
-	@Override public String getName(@NonNull Context c) { return ""; }
+	@Override @Nullable public String getName(@Nullable Context c) { return ""; }
 }
